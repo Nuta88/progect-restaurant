ddffbfab8763f4a0763b72f7e2f914d2ba9f8b13
gvagntc <gvagntc@chclass11.training.local>
2017-09-07 18:12:07 +0300

    calendar

--- src/app/form-add-dish/form-add-dish.component.ts
+++ src/app/form-add-dish/form-add-dish.component.ts
@@ -16,9 +16,33 @@ export class FormAddDishComponent {
 
   myForm: FormGroup;
   date1: Date;
-  date2: Date;
-selectDate(date){ this.date2 = date; console.log(this.date2.getDate());}
+  date2: string;
+  minDate: Date;
+  selectDate(date) {
+    let del = 6 - date.getDay();
+    this.date2 = ((date.getDate() + del)+".0"+ (date.getMonth() + 1) + "." + date.getFullYear());
+    /**let list: number[] = [Number(date.getFullYear()), Number((date.getDate() + del)), Number("0" + (date.getMonth() + 1))];
+    this.date2 = new Date(list[0], list[2] - 1, list[1]);*/
+  }
+  en: any;
 
+  ngOnInit() {
+
+    let today = new Date();
+    this.minDate = new Date();
+    this.minDate.setDate(today.getDate());
+
+    this.en = {
+      firstDayOfWeek: 0,
+      dayNames: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+      dayNamesShort: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      dayNamesMin: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+      monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      today: 'Today',
+      clear: 'Clear'
+    };
+  }
 
   constructor() {
     this.myForm = new FormGroup({
